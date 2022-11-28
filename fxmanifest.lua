2d151c76a2176b3fc98eda632c2e719f26acbc62
aymannajim <36258300+aymannajim@users.noreply.github.com>
2022-11-28 10:34:42 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@
 -- The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
------ Made By https://aymantv.tebex.io/
+----- Made By https://a-n.tebex.io/
 ----- For Support, Join my Discord: https://discord.gg/f2Nbv9Ebf5
 ----- For custom services or help, check my Fiverr: https://www.fiverr.com/aymannajim
 
@@ -22,6 +22,6 @@ shared_scripts {
 	'config.lua',
 }
 
------ Made By https://aymantv.tebex.io/
+----- Made By https://a-n.tebex.io/
 ----- For Support, Join my Discord: https://discord.gg/f2Nbv9Ebf5
 ----- For custom services or help, check my Fiverr: https://www.fiverr.com/aymannajim
